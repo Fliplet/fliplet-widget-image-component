6ca2b64d4cb2d5236d3c5b2b1a4062a3335053cf
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-01-23 18:18:32 +0100

    image component in progress

---
+++ js/build.js
@@ -0,0 +1,123 @@
+// Register this widget instance
+Fliplet.Widget.instance({
+  name: 'image-component',
+  displayName: 'Image component',
+  render: {
+    template: ['<div class="image-component-container"></div>'].join(''),
+    ready: function() {
+      // Initialize children components when this widget is ready
+      Fliplet.Widget.initializeChildren(this.$el, this);
+
+      const imageComponent = this;
+      const $imageComponent = $(this);
+      const placeholderPath = '../img/placeholder.jpg';
+      const $imageContainer = $imageComponent.find(
+        '.image-component-container'
+      );
+
+      let finalImage;
+      let dataSourceId = null;
+      let selectedImageColumn = null;
+      let noImageFound = null;
+
+      imageComponent.fields = _.assign(
+        {
+          noImageFound: 'Placeholder',
+          imageColumn: ''
+        },
+        imageComponent.fields
+      );
+      selectedImageColumn = imageComponent.fields.imageColumn;
+      noImageFound = imageComponent.fields.noImageFound;
+
+      if (!Fliplet.DynamicContainer) {
+        return showToastMessage(
+          'Please add dynamic container and configure it'
+        );
+      }
+
+      Fliplet.DynamicContainer.get().then(function(container) {
+        return container.connection().then(function(connection) {
+          dataSourceId = connection.id;
+
+          if (!dataSourceId) {
+            return showToastMessage(
+              'Please select data source from dynamic container'
+            );
+          }
+
+          if (selectedImageColumn) {
+            return showToastMessage('Please select column');
+          }
+
+          // Detail view
+          Fliplet.Hooks.on('recordContainerDataRetrieved', function(record) {
+            let imageColumnValue = record.entry[selectedImageColumn];
+            let imageValue = null;
+
+            if (imageColumnValue) {
+              if (Array.isArray(imageColumnValue) && imageColumnValue.length) {
+                imageValue = imageColumnValue[0];
+              } else {
+                imageValue = imageColumnValue;
+              }
+            }
+
+            manageImage(imageValue);
+          });
+
+          // List repeater
+          // Fliplet.Hooks.on('repeaterDataRetrieved', function(options) {
+          //   options.data.length
+          //   options.data.map(el => el.id)
+          //   options.data[0].dataSourceId
+          // });
+        });
+      });
+
+      function manageImage(imageValue) {
+        if (!imageValue && noImageFound === 'Placeholder') {
+          finalImage = `<img src="${placeholderPath}" alt="Image component" />`;
+        } else {
+          finalImage = `<img src="${Fliplet.Media.authenticate(
+            imageValue
+          )}" alt="Image component" />`;
+        }
+
+        if (finalImage) {
+          $imageContainer.html(finalImage);
+        }
+      }
+
+      // function manageImageAsArray(arrayValues) {
+      //   let images = [];
+
+      //   for (let i = 0; i < arrayValues.length; i++) {
+      //     let src = arrayValues[i];
+
+      //     if (src) {
+      //       src = Fliplet.Media.authenticate(src);
+      //     } else if (noImageFound === 'Placeholder') {
+      //       src = placeholderPath;
+      //     } else {
+      //       continue;
+      //     }
+
+      //     let image = $('<img>', {
+      //       class: 'image-component',
+      //       src,
+      //       alt: 'Image component'
+      //     });
+
+      //     images.push(image);
+      //   }
+
+      //   $imageContainer.append(images);
+      // }
+
+      function showToastMessage(message) {
+        return Fliplet.UI.Toast(message);
+      }
+    }
+  }
+});
---
+++ js/interface.js
@@ -0,0 +1,75 @@
+// // This function is used to generate the interface for the widget
+var dataSourceColumns = [];
+
+Fliplet.Widget.generateInterface({
+  title: 'Image component',
+  fields: [
+    {
+      type: 'html',
+      html: `<header>
+              <p>
+                Configure image
+              <a
+                href="https://help.fliplet.com/image-component/"
+                class="help-icon"
+                target="_blank"
+              >
+                <i class="fa fa-question-circle-o"></i>
+              </a>
+              </p>
+            </header>
+            <hr/>`
+    },
+    {
+      type: 'html',
+      html: `<div>
+              <h3 class="font-weight-bold">Get image from</h3>
+              <p>
+                <span class="ds-id">xxxxxx</span>
+                <span class="ds-name">xxxx xxxx xxxx</span>
+              </p>
+            </div>`
+    },
+    {
+      name: 'imageColumn',
+      type: 'dropdown',
+      label: 'Select column',
+      options: dataSourceColumns,
+      default: '',
+      ready: async function() {
+        if (Fliplet.DynamicContainer) {
+          return await Fliplet.DynamicContainer.get().then(function(
+            container
+          ) {
+            return container.connection().then(function(connection) {
+              return connection.id;
+            }).then((dataSourceId) => {
+              return Fliplet.DataSources.getById(dataSourceId, {
+                attributes: ['columns']
+              }).then(async function(dataSource) {
+                dataSourceColumns = dataSource.columns.map((el) => {
+                  return {
+                    id: el,
+                    label: el
+                  };
+                });
+
+                return Promise.resolve(true);
+              });
+            });
+          });
+        }
+      }
+    },
+    {
+      name: 'noImageFound',
+      type: 'radio',
+      label: 'If no image found',
+      options: [
+        { value: 'Placeholder', label: 'Show placeholder' },
+        { value: 'None', label: 'None' }
+      ],
+      default: 'Placeholder'
+    }
+  ]
+});
