160bfde6b9cbe93c9d41579d87ff89cf0a6c848f
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-01-23 19:25:31 +0100

    image component in progress

--- js/interface.js
+++ js/interface.js
@@ -1,12 +1,22 @@
 // // This function is used to generate the interface for the widget
 var dataSourceColumns = [];
 
-Fliplet.Widget.generateInterface({
-  title: 'Image component',
-  fields: [
-    {
-      type: 'html',
-      html: `<header>
+return Fliplet.DataSources.getById(702725, {
+  attributes: ['columns']
+}).then(async function(dataSource) {
+  dataSourceColumns = dataSource.columns.map((el) => {
+    return {
+      id: el,
+      label: el
+    };
+  });
+
+  Fliplet.Widget.generateInterface({
+    title: 'Image component',
+    fields: [
+      {
+        type: 'html',
+        html: `<header>
               <p>
                 Configure image
               <a
@@ -19,59 +29,34 @@ Fliplet.Widget.generateInterface({
               </p>
             </header>
             <hr/>`
-    },
-    {
-      type: 'html',
-      html: `<div>
+      },
+      {
+        type: 'html',
+        html: `<div>
               <h3 class="font-weight-bold">Get image from</h3>
               <p>
                 <span class="ds-id">xxxxxx</span>
                 <span class="ds-name">xxxx xxxx xxxx</span>
               </p>
             </div>`
-    },
-    {
-      name: 'imageColumn',
-      type: 'dropdown',
-      label: 'Select column',
-      options: dataSourceColumns,
-      default: '',
-      ready: async function() {
-        // if (Fliplet.DynamicContainer) {
-        //   return await Fliplet.DynamicContainer.get().then(function(
-        //     container
-        //   ) {
-        //     return container.connection().then(function(connection) {
-        //       return connection.id;
-        //     }).then((dataSourceId) => {
-        return Fliplet.DataSources.getById(702725, {
-          attributes: ['columns']
-        }).then(async function(dataSource) {
-          dataSourceColumns = dataSource.columns.map((el) => {
-            return {
-              id: el,
-              label: el
-            };
-          });
-          // Fliplet.Helper.field('imageColumn');
-          debugger;
-
-          return Promise.resolve(true);
-        });
-        // });
-        //   });
-        // }
+      },
+      {
+        name: 'imageColumn',
+        type: 'dropdown',
+        label: 'Select column',
+        options: dataSourceColumns,
+        default: ''
+      },
+      {
+        name: 'noImageFound',
+        type: 'radio',
+        label: 'If no image found',
+        options: [
+          { value: 'Placeholder', label: 'Show placeholder' },
+          { value: 'None', label: 'None' }
+        ],
+        default: 'Placeholder'
       }
-    },
-    {
-      name: 'noImageFound',
-      type: 'radio',
-      label: 'If no image found',
-      options: [
-        { value: 'Placeholder', label: 'Show placeholder' },
-        { value: 'None', label: 'None' }
-      ],
-      default: 'Placeholder'
-    }
-  ]
+    ]
+  });
 });
