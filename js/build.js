f053c7d9c744905c0b0c8f26bd9c4b6e4e2fe9fe
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-01-24 09:58:04 +0100

    image component in progress

--- js/build.js
+++ js/build.js
@@ -4,9 +4,9 @@ Fliplet.Widget.instance({
   displayName: 'Image component',
   render: {
     template: ['<div class="image-component-container"></div>'].join(''),
-    ready: function() {
+    ready: async function() {
       // Initialize children components when this widget is ready
-      Fliplet.Widget.initializeChildren(this.$el, this);
+      await Fliplet.Widget.initializeChildren(this.$el, this);
 
       const imageComponent = this;
       // const $imageComponent = $(this);
@@ -37,45 +37,48 @@ Fliplet.Widget.instance({
       }
 
       // Detail view
-      Fliplet.Hooks.on('recordContainerDataRetrieved', function(record) {
-        Fliplet.DynamicContainer.get().then(function(container) {
-          return container.connection().then(function(connection) {
-            dataSourceId = connection.id;
+      await Fliplet.DynamicContainer.get().then(function(container) {
+        return container.connection().then(function(connection) {
+          dataSourceId = connection.id;
 
-            if (!dataSourceId) {
-              return showToastMessage(
-                'Please select data source from dynamic container'
-              );
-            }
+          if (!dataSourceId) {
+            return showToastMessage(
+              'Please select data source from dynamic container'
+            );
+          }
 
-            if (!selectedImageColumn) {
-              return showToastMessage('Please select column');
-            }
-
-            let imageColumnValue = record.entry.data[selectedImageColumn];
-            let imageValue = null;
-
-            if (imageColumnValue) {
-              if (Array.isArray(imageColumnValue)) {
-                if (imageColumnValue.length) {
-                  imageValue = imageColumnValue[0];
-                }
-              } else {
-                imageValue = imageColumnValue;
-              }
-            }
-
-            return manageImage(imageValue);
-          });
-
-          // List repeater
-          // Fliplet.Hooks.on('repeaterDataRetrieved', function(options) {
-          //   options.data.length
-          //   options.data.map(el => el.id)
-          //   options.data[0].dataSourceId
-          // });
+          return Promise.resolve(true);
         });
       });
+
+
+      Fliplet.Hooks.on('recordContainerDataRetrieved', function(record) {
+        if (!selectedImageColumn) {
+          return showToastMessage('Please select column');
+        }
+
+        let imageColumnValue = record.entry.data[selectedImageColumn];
+        let imageValue = null;
+
+        if (imageColumnValue) {
+          if (Array.isArray(imageColumnValue)) {
+            if (imageColumnValue.length) {
+              imageValue = imageColumnValue[0];
+            }
+          } else {
+            imageValue = imageColumnValue;
+          }
+        }
+
+        return manageImage(imageValue);
+      });
+
+      // List repeater
+      // Fliplet.Hooks.on('repeaterDataRetrieved', function(options) {
+      //   options.data.length
+      //   options.data.map(el => el.id)
+      //   options.data[0].dataSourceId
+      // });
 
       function manageImage(imageValue) {
         if (!imageValue && noImageFound === 'Placeholder') {
