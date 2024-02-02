b411eefaffaa70b502a66e16fec90a3d14cba546
Veljko Popović <veljko_popovic_90@hotmail.com>
2024-02-02 08:06:22 +0100

    image component

--- js/interface.js
+++ js/interface.js
@@ -1,58 +1,73 @@
 // // This function is used to generate the interface for the widget
 var dataSourceColumns = [];
 // TODO check how to get dataSourceId
+var dataSourceId = null;
 
-Fliplet.DataSources.getById(702725, {
-  attributes: ['columns']
-}).then(async function(dataSource) {
-  dataSourceColumns = dataSource.columns;
+if (Fliplet.DynamicContainer) {
+  Fliplet.DynamicContainer.get().then(function(container) {
+    return container.connection().then(function(connection) {
+      dataSourceId = connection.id;
 
-  Fliplet.Widget.generateInterface({
-    title: 'Image component',
-    fields: [
-      {
-        type: 'html',
-        html: `<header>
-              <p>
-                Configure image
-              <a
-                href="https://help.fliplet.com/image-component/"
-                class="help-icon"
-                target="_blank"
-              >
-                <i class="fa fa-question-circle-o"></i>
-              </a>
-              </p>
-            </header>
-            <hr/>`
-      },
-      {
-        type: 'html',
-        html: `<div>
-              <h3 class="font-weight-bold">Get image from</h3>
-              <p>
-                <span class="ds-id">xxxxxx</span>
-                <span class="ds-name">xxxx xxxx xxxx</span>
-              </p>
-            </div>`
-      },
-      {
-        name: 'imageColumn',
-        type: 'dropdown',
-        label: 'Select column',
-        options: dataSourceColumns,
-        default: ''
-      },
-      {
-        name: 'noImageFound',
-        type: 'radio',
-        label: 'If no image found',
-        options: [
-          { value: 'Placeholder', label: 'Show placeholder' },
-          { value: 'None', label: 'None' }
-        ],
-        default: 'Placeholder'
+      if (dataSourceId) {
+        Fliplet.DataSources.getById(dataSourceId, {
+          attributes: ['columns']
+        }).then(async function(dataSource) {
+          dataSourceColumns = dataSource.columns;
+
+          Fliplet.Widget.generateInterface({
+            title: 'Image component',
+            fields: [
+              {
+                type: 'html',
+                html: `<header>
+                <p>
+                  Configure image
+                <a
+                  href="https://help.fliplet.com/image-component/"
+                  class="help-icon"
+                  target="_blank"
+                >
+                  <i class="fa fa-question-circle-o"></i>
+                </a>
+                </p>
+              </header>
+              <hr/>`
+              },
+              {
+                type: 'html',
+                html: `<div>
+                <h3 class="font-weight-bold">Get image from</h3>
+                <p>
+                  <span class="ds-id">xxxxxx</span>
+                  <span class="ds-name">xxxx xxxx xxxx</span>
+                </p>
+              </div>`
+              },
+              {
+                name: 'imageColumn',
+                type: 'dropdown',
+                label: 'Select column',
+                options: dataSourceColumns,
+                default: ''
+              },
+              {
+                name: 'noImageFound',
+                type: 'radio',
+                label: 'If no image found',
+                options: [
+                  { value: 'Placeholder', label: 'Show placeholder' },
+                  { value: 'None', label: 'None' }
+                ],
+                default: 'Placeholder'
+              }
+            ]
+          });
+        });
+      } else {
+        Fliplet.UI.Toast('Config Dynamic container');
       }
-    ]
+    });
   });
-});
+} else {
+  Fliplet.UI.Toast('Add and config Dynamic container');
+}
